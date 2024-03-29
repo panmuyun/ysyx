f0748b494f06795955de64024f81eb429899b735
tracer-ysyx <tracer@ysyx.org>
2024-03-29 10:57:34 +0800

    > sim RTL ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:57:34 up 44 min,  1 user,  load average: 0.21, 0.24, 0.29

--- npc/simcsrc/top_main.cpp
+++ npc/simcsrc/top_main.cpp
@@ -1,44 +1,51 @@
-
-#include "Vtop.h"
+#include "Vtop.h" 
 #include "verilated.h"
-#include "verilated_vcd_c.h"
+#include "verilated_vcd_c.h" 
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-  
-int main(int argc, char** argv) {
-    VerilatedContext* contextp = new VerilatedContext;
+
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static Vtop* top;
+
+void sim_init(){
+    contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
-    
-    Vtop* top = new Vtop{contextp};
-    
-    VerilatedVcdC *tfp= new VerilatedVcdC; 
-    contextp->traceEverOn(true);
+    top = new Vtop{contextp};
+    tfp= new VerilatedVcdC;
+    contextp->traceEverOn(true); //打开追踪
     top->trace(tfp,0);
-    tfp->open("wave.vcd");
-    
+    tfp->open("wave.vcd"); //保存位置
+}
+
+void step_and_dump_wave(){  
+    top->eval();  
+    tfp->dump(contextp->time()); //dump wave
+    contextp->timeInc(1); //仿真时间推进
+}
+
+void sim_exit(){
+    delete top;
+    tfp->close();
+    delete contextp;
+}
+int main(int argc, char** argv) {
+    sim_init();
     int cycle=0;
-    
     while (!contextp->gotFinish()) {
-        if(cycle==20)
+        if(cycle==20)   //设定最长时钟周期
             break;
         int a = rand() &1;
         int b = rand() &1;
         top->a=a;
         top->b=b;
-        top->eval();
+        step_and_dump_wave();
         printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-        
-        tfp->dump(contextp->time());
-        contextp->timeInc(1);
-        
         assert(top->f == (a ^ b));
         cycle++;
     }
-    
-    delete top;
-    tfp->close();
-    delete contextp;
+    sim_exit();
     return 0;
 }
-
