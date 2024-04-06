377a3f658e3e33fb438f85abeb314b0f1adead36
tracer-ysyx <tracer@ysyx.org>
2024-04-06 17:23:29 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:23:29 up  6:34,  1 user,  load average: 0.49, 0.39, 0.35

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -101,7 +101,7 @@ static int cmd_x(char *args) {//扫描内存
   for(uint64_t i=0;i<n;i++){
     word_t value = vaddr_read(addr, 4);
     printf("0x%08x\t\t0x%08x\t\t",addr,value);
-    printf("%02x %02x %02x %02x\n",(value >> 24) & 0xFF, (value >> 16) & 0xFF, (value >> 8) & 0xFF, value & 0xFF);
+    printf("%02x %02x %02x %02x\n", value & 0xFF, (value >> 8) & 0xFF, (value >> 16) & 0xFF, (value >> 24) & 0xFF);
     addr += 4;
   }
   return 0;
