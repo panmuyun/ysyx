f9fd353d4191b5d24484643aec40da70dcfc43fd
tracer-ysyx <tracer@ysyx.org>
2024-04-08 17:03:37 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:37 up  4:41,  1 user,  load average: 0.73, 0.56, 0.49

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -98,7 +98,8 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        if(tokens[nr_token-1].type==TK_NUMBER && *substr_start=='-'){ //上一个token是数字 且 当前获取了一个‘-’
+        if( ( tokens[nr_token-1].type==TK_NUMBER || tokens[nr_token-1].type=='(' ||  tokens[nr_token-1].type==')' )
+        && *substr_start=='-'){ //上一个token是数字 且 当前获取了一个‘-’
           substr_len=1;
           Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, "\\-", position, substr_len, substr_len, substr_start);
