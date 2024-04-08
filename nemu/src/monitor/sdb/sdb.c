b62fc6d6a88958d9ce194a9953d1af0724fec0b4
tracer-ysyx <tracer@ysyx.org>
2024-04-08 11:15:34 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:15:34 up 34 min,  1 user,  load average: 0.84, 0.48, 0.39

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -109,7 +109,8 @@ static int cmd_x(char *args) {//扫描内存
 
 static int cmd_p(char *args) {//表达式求值
   bool success=NULL;
-  expr(args, &success);
+  word_t exprvalue = expr(args, &success);
+  printf("value of the expression = %u\n", exprvalue);
   assert(success==true);
   return 0;
 }
