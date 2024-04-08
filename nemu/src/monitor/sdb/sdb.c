4e4909ecbac5df8848780bd5ecb81ae846fc0694
tracer-ysyx <tracer@ysyx.org>
2024-04-08 11:41:41 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:41:41 up  1:00,  1 user,  load average: 0.60, 0.48, 0.43

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -91,24 +91,24 @@ static int cmd_x(char *args) {//扫描内存
   }
   Assert(hexnum != NULL, "scan memory: Input invalid EXPR");
 
-  bool success=NULL;
-  word_t exprvalue = expr(hexnum, &success);
-  printf("value of the expression = %u\n", exprvalue);
-  assert(success==true);
+  // bool success=NULL;
+  // word_t exprvalue = expr(hexnum, &success);
+  // printf("value of the expression = %u\n", exprvalue);
+  // assert(success==true);
 
-  // vaddr_t addr;
-  // char *endptr;
-  // if(hexnum!=NULL){
-  //   addr = (vaddr_t)strtol(hexnum, &endptr, 0);
-  // }
+  vaddr_t addr;
+  char *endptr;
+  if(hexnum!=NULL){
+    addr = (vaddr_t)strtol(hexnum, &endptr, 0);
+  }
   //printf("addr=%08x\n",addr);
 
   printf("Address\t\tDword block\tByte sequence\n");
   for(uint64_t i=0;i<n;i++){
-    word_t value = vaddr_read(exprvalue, 4);
-    printf("0x%08x\t0x%08x\t",exprvalue,value);
+    word_t value = vaddr_read(addr, 4);
+    printf("0x%08x\t0x%08x\t",addr,value);
     printf("%02x %02x %02x %02x\n", value & 0xFF, (value >> 8) & 0xFF, (value >> 16) & 0xFF, (value >> 24) & 0xFF);
-    exprvalue += 4;
+    addr += 4;
   }
   return 0;
 }
