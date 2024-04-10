e3f178f2e35e08c5e6d0655d62fe142e9646e961
tracer-ysyx <tracer@ysyx.org>
2024-04-10 22:21:05 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:21:05 up  6:42,  1 user,  load average: 0.52, 0.56, 0.49

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -253,7 +253,7 @@ int eval(int p, int q){
         int r;
         int regs_num = ARRLEN(registers);
         for(r=0;r<regs_num;r++){
-          if(strcmp(registers[r], regname)){
+          if(strcmp(registers[r], regname)==0){
             printf("registers[r] : %s\n", registers[r]);
             printf("reg_index : %d\n", r);
             return (int)gpr(r);  //寄存器中的值
