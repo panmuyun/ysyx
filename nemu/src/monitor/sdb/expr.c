339f5bcd0ab75caf396288287a88914edab5c18b
tracer-ysyx <tracer@ysyx.org>
2024-04-10 22:17:30 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:17:30 up  6:39,  1 user,  load average: 0.67, 0.57, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -252,8 +252,10 @@ int eval(int p, int q){
         int r;
         int regs_num = ARRLEN(registers);
         for(r=0;r<regs_num;r++){
-          if(strcmp(registers[r], regname))
+          if(strcmp(registers[r], regname)){
+            printf("reg_index : %d\n", r);
             return (int)gpr(r);  //寄存器中的值
+          }
         }
         Assert(r<regs_num, "regitser name isn't exist");
         return -1;
