5bd190f2db63cb5b2c1df17a5dcbda0fa5481d84
tracer-ysyx <tracer@ysyx.org>
2024-04-10 18:00:20 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:00:20 up  2:21,  1 user,  load average: 0.82, 0.43, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -101,8 +101,8 @@ static bool make_token(char *e) {
         if( ( tokens[nr_token-1].type==TK_NUMBER || tokens[nr_token-1].type=='(' ||  tokens[nr_token-1].type==')' )
         && *substr_start=='-'){ //上一个token是数字 且 当前获取了一个‘-’
           substr_len=1;
-          Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, "\\-", position, substr_len, substr_len, substr_start);
+          // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+          //   i, "\\-", position, substr_len, substr_len, substr_start);
           position += substr_len;
           tokens[nr_token].type='-';
           strcpy(tokens[nr_token].str, "-");
@@ -110,8 +110,8 @@ static bool make_token(char *e) {
           break;
         }
         
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        //     i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
 
