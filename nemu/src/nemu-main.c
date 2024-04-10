a746e9a7f9c139fac6fc6b4e09b2f044e5765fb2
tracer-ysyx <tracer@ysyx.org>
2024-04-10 15:51:13 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:13 up 12 min,  1 user,  load average: 0.46, 0.50, 0.38

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -29,16 +29,16 @@ int main(int argc, char *argv[]) {
   while(scanf("%d %[^\n]", &num, expressions)!=EOF){
     linenum++;
     printf("line %d : %s\n", linenum, expressions);
-    char *e = expressions;
-    printf("%s\n", e);
+    //char *e = expressions;
+    //printf("%s\n", e);
     //expr(e, NULL);
 
     //printf("line %d : %d\n", linenum, val );
-
-    // if(num == expr(expressions, NULL)){
-    //   printf("line %d : YES!!\n", linenum);
-    // }else
-    //   printf("line %d : NO-------------------\n", linenum);
+    bool success;
+    if(num == expr(expressions, &success)){
+      printf("line %d : YES!!\n", linenum);
+    }else
+      printf("line %d : NO-------------------\n", linenum);
 
   }
 
