6a73027f375e945ade601f821b4145b668c5e23b
tracer-ysyx <tracer@ysyx.org>
2024-04-11 14:35:08 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:35:08 up  1:15,  1 user,  load average: 0.48, 0.31, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -169,13 +169,6 @@ static bool make_token(char *e) {
 
 
 
-// const char *registers[] = {
-//   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-//   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-//   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-//   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-// };
-
 bool check_parentheses(int p, int q){
   if(tokens[p].type=='(' && tokens[q].type==')'){
     int left_parenthese = 0;
@@ -288,7 +281,6 @@ EXPR_value_TYPE eval(int p, int q){
       }
     }
   }
-  // return -1;
 }
 
 EXPR_value_TYPE expr(char *e, bool *success) {
