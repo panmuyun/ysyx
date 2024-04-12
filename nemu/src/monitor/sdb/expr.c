955f9749b7c526dda3275ecb0a23b3d800935feb
tracer-ysyx <tracer@ysyx.org>
2024-04-12 16:57:03 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:57:03 up  4:45,  1 user,  load average: 0.33, 0.26, 0.26

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -297,7 +297,7 @@ EXPR_value_TYPE expr(char *e, bool *success) {
   for (int i = 0; i < length_tokens; i++){
     if (tokens[i].type != 0){
       q++;
-      printf("tokens[%d].type = %d  ;  str = %s\n", i, tokens[i].type, tokens[i].str);
+      //printf("tokens[%d].type = %d  ;  str = %s\n", i, tokens[i].type, tokens[i].str);
     }else
       break;
   }
--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -98,7 +98,7 @@ bool watchpoints_hit(){
     p->Newval = expr(p->What, &success);
     if(p->Newval != p->Oldval){
       p->Oldval = p->Newval; 
-      Log("Hit watchpoint %d : %s ", p->NO, p->What);//at address 0x%08x
+      Log("\033[0;33m Hit watchpoint %d : %s \033[0m", p->NO, p->What);//at address 0x%08x
       break;     
     }
     p = p->next;
