c37cb0def15c87d9702106b9b9712943263cdee5
tracer-ysyx <tracer@ysyx.org>
2024-04-12 15:30:40 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:30:39 up  3:19,  1 user,  load average: 0.24, 0.27, 0.26

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -83,18 +83,12 @@ void free_wp(WP *wp){
 }
 
 void watchpoints_display(){
-  // WP *p = head;
-  // while (p != NULL){
-  //   bool success;
-  //   p->Newval = expr(p->What, &success);
-  //   if(p->Newval != p->Oldval){
-  //     nemu_state.state = NEMU_STOP;  
-  //     p->Oldval = p->Newval; 
-  //     printf("Hit watchpoint %d \n", p->NO);//at address 0x%08x
-  //     break;     
-  //   }
-  //   p = p->next;
-  // }
+  WP *p = head;
+  printf("NO\tExpr\t\tOld value\n");
+  while (p != NULL){
+    printf("%d\t%s\t\t0x%08x\n", p->NO, p->What, p->Oldval);
+    p = p->next;
+  }
 }
 
 WP* watchpoints_check(){
--- nemu/src/monitor/sdb/watchpoint.h
+++ nemu/src/monitor/sdb/watchpoint.h
@@ -1,7 +1,7 @@
 #ifndef __WATCHPOINT_H__
 #define __WATCHPOINT_H__
 
-#include "common.h"
+#include <common.h>
 #include "sdb.h"
 
 #define NR_WP 32
