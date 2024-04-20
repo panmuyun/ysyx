89254ceb22dbcfd830d23235c4ed04e95c799d23
tracer-ysyx <tracer@ysyx.org>
2024-04-20 12:29:34 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:29:34 up  3:15,  1 user,  load average: 0.84, 0.71, 0.84

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -26,8 +26,8 @@ static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
+"  int result = %s; "
+"  printf(\"%%d\", result); "
 "  return 0; "
 "}";
 
