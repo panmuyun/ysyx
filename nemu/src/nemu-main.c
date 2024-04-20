c5ad669357fd90de8d6abe4473f6bfdafad4ca93
tracer-ysyx <tracer@ysyx.org>
2024-04-20 12:10:12 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:10:12 up  2:56,  1 user,  load average: 0.54, 0.88, 0.91

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
   int linenum=0;
 
   while(1){ //
-    char expressions[700];
+    char expressions[600];
     if(scanf("%d %[^\n]", &num, expressions)==EOF)
       break;
     linenum++;
