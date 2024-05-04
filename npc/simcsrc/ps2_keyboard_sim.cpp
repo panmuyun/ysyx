a345acdf5b72453c8cd73387c14ca2785ee35a11
tracer-ysyx <tracer@ysyx.org>
2024-05-04 22:38:50 +0800

    > sim RTL ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:38:50 up  6:43,  1 user,  load average: 0.55, 0.45, 0.46

--- npc/simcsrc/ps2_keyboard_sim.cpp
+++ npc/simcsrc/ps2_keyboard_sim.cpp
@@ -35,7 +35,7 @@ void sim_exit(){
 
 int kbd_clk_period = 60;
 
-void kbd_sendcode(std::string code){
+void kbd_sendcode(std::string code, std::string *send_buffer){
     int i;
     //bitset<1> startbit(0);
     std::bitset<8> databits(code);
@@ -43,26 +43,28 @@ void kbd_sendcode(std::string code){
     for(int k=1;k<8;k++)
         oddparitybit = oddparitybit^std::bitset<1>(databits.test(k)?1:0);
     //bitset<1> stopbit(1);
-    std::string send_buffer = "0"+code+oddparitybit.to_string()+"1";
-    i=0;
-    while(i<11){
-        top->ps2_data = send_buffer[i]=='1'? 1 : 0;
+    send_buffer = "0"+code+oddparitybit.to_string()+"1";
+    // i=0;
+    // while(i<11){
+    //     top->ps2_data = send_buffer[i]=='1'? 1 : 0;
 
-        contextp->timeInc(kbd_clk_period/2); 
-        top->ps2_clk = 0;
-        top->eval();
-        tfp->dump(contextp->time()); //dump wave
+    //     contextp->timeInc(kbd_clk_period/2); 
+    //     top->ps2_clk = 0;
+    //     top->eval();
+    //     tfp->dump(contextp->time()); //dump wave
 
-        contextp->timeInc(kbd_clk_period/2);
-        top->ps2_clk = 1;
-        top->eval();
-        tfp->dump(contextp->time()); //dump wave
-    }
+    //     contextp->timeInc(kbd_clk_period/2);
+    //     top->ps2_clk = 1;
+    //     top->eval();
+    //     tfp->dump(contextp->time()); //dump wave
+    // }
 }
 
 int main(int argc, char** argv) {
     sim_init(argc, argv);
     int cycle=0;
+    std::string send_buffer="";
+    int i=0;
 
     top->clk = 0;
     top->resetn = 0;
@@ -77,16 +79,32 @@ int main(int argc, char** argv) {
         
         contextp->timeInc(5);
         top->clk = !top->clk;
+        if(i==11){
+            i=0;
+        }
+        if(i==0)
+            kbd_sendcode("00011100", &send_buffer);
+        top->ps2_data = send_buffer[i]=='1'? 1 : 0;
 
-        // kbd_sendcode("00011100");
+        contextp->timeInc(kbd_clk_period/2); 
+        top->ps2_clk = 0;
+        top->eval();
+        tfp->dump(contextp->time()); //dump wave
+
+        contextp->timeInc(kbd_clk_period/2);
+        top->ps2_clk = 1;
+        top->eval();
+        tfp->dump(contextp->time()); //dump wave
+
+        i++;
         // kbd_sendcode("11110000");
         // kbd_sendcode("00011100");
         // kbd_sendcode("00011011");
         // kbd_sendcode("11110000");
         // kbd_sendcode("00011011");
         
-        top->eval();
-        tfp->dump(contextp->time()); //dump wave
+        // top->eval();
+        // tfp->dump(contextp->time()); //dump wave
         // printf("clk = %d, resetn = %d, ps2_clk = %d, ps2_data = %d\n", top->clk, top->resetn, top->ps2_clk, top->ps2_data);
         //assert(top->f == (a ^ b));
         cycle++;
