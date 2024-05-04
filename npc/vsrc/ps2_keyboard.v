26b89edd292d6473b415ac8b20d6ed9636a02d01
tracer-ysyx <tracer@ysyx.org>
2024-05-04 23:34:19 +0800

    > sim RTL ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:34:19 up  7:38,  1 user,  load average: 0.20, 0.29, 0.33

--- npc/vsrc/ps2_keyboard.v
+++ npc/vsrc/ps2_keyboard.v
@@ -26,7 +26,6 @@ module ps2_keyboard(
     always @(posedge clk) begin
         if (resetn == 0) begin // reset
             count <= 0;
-            data <= 0;
         end
         else begin
             if (sampling) begin //位有效
