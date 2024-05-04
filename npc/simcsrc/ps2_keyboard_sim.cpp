a05796509a3ead92dfebf58a06343198ddf309a8
tracer-ysyx <tracer@ysyx.org>
2024-05-04 17:55:59 +0800

    > sim RTL ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:55:59 up  2:00,  1 user,  load average: 0.72, 0.38, 0.30

--- npc/simcsrc/ps2_keyboard_sim.cpp
+++ npc/simcsrc/ps2_keyboard_sim.cpp
@@ -32,32 +32,48 @@ void sim_exit(){
     tfp->close();
     delete contextp;
 }
-// void kbd_sendcode(bitset<8> code){
-//     int i;
-//     //bitset<1> startbit(0);
-//     bitset<1> oddparitybit=code[0];
-//     for(int k=1;k<8;k++)
-//         oddparitybit = oddparitybit ^ code[k];
-//     //bitset<1> stopbit(1);
-//     bitset<11> send_buffer("0"+code.to_string()+oddparitybit.to_string()+"1");
-//     i=10;
-//     while(i<11){
-//         top->ps2_data = send_buffer[i];
 
-//     }
-// }
+int kbd_clk_period = 60;
+
+void kbd_sendcode(bitset<8> code){
+    int i;
+    //bitset<1> startbit(0);
+    bitset<1> oddparitybit=code[0];
+    for(int k=1;k<8;k++)
+        oddparitybit = oddparitybit ^ code[k];
+    //bitset<1> stopbit(1);
+    bitset<11> send_buffer("0"+code.to_string()+oddparitybit.to_string()+"1");
+    i=0;
+    while(i<11){
+        top->ps2_data = send_buffer[i];
+        for (int t = 0; t < kbd_clk_period/2; t++)
+            step_and_dump_wave();
+        top->ps2_clk = 0;
+        for (int t = 0; t < kbd_clk_period/2; t++)
+            step_and_dump_wave();
+        top->ps2_clk = 1;
+    }
+}
+
 int main(int argc, char** argv) {
     sim_init(argc, argv);
     int cycle=0;
     while (!contextp->gotFinish()) {
         if(cycle==30)   //设定最长时钟周期
             break;
-        //int a = rand() & ((1<<位数) -1);
-        //int b = rand() & ((1<<位数) -1);
-        //top->a=a;
-        //top->b=b;
-
-        step_and_dump_wave();
+        top->resetn = 0;
+        for (int t = 0; t < 20; t++)
+            step_and_dump_wave();
+        top->resetn = 1;
+        for (int t = 0; t < 20; t++)
+            step_and_dump_wave();
+        kbd_sendcode("00011100");
+        kbd_sendcode("11110000");
+        kbd_sendcode("00011100");
+        kbd_sendcode("00011011");
+        kbd_sendcode("11110000");
+        kbd_sendcode("00011011");
+        // step_and_dump_wave();
         //printf("a = %d, b = %d, f = %d\n", a, b, top->f);
         //assert(top->f == (a ^ b));
         cycle++;
