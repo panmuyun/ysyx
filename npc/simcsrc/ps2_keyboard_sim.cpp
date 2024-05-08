eb9c6a5ae8fee8e75e2b4c87e07fc3e4f83af07c
tracer-ysyx <tracer@ysyx.org>
2024-05-08 10:06:06 +0800

    > sim RTL ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:06:06 up  1:13,  1 user,  load average: 0.35, 0.42, 0.37

--- npc/simcsrc/ps2_keyboard_sim.cpp
+++ npc/simcsrc/ps2_keyboard_sim.cpp
@@ -73,7 +73,7 @@ int main(int argc, char** argv) {
         contextp->timeInc(5);
         top->clk = !top->clk;
         top->eval();
-        // tfp->dump(contextp->time()); //dump wave
+        tfp->dump(contextp->time()); //dump wave
 
         if(i==11)
             i=0;
@@ -82,24 +82,25 @@ int main(int argc, char** argv) {
             kbd_sendcode(vt[v++], &send_buffer);
         top->ps2_data = send_buffer[i]=='1'? 1 : 0;
         top->eval();
+        tfp->dump(contextp->time()); //dump wave
 
         for(int t=0;t<3;t++){
             contextp->timeInc(5);
             top->clk = !top->clk;
             top->eval();
-            // tfp->dump(contextp->time()); //dump wave
+            tfp->dump(contextp->time()); //dump wave
         }
         contextp->timeInc(5);
         top->clk = !top->clk;
         top->ps2_clk = 0;
         top->eval();
-        // tfp->dump(contextp->time()); //dump wave
+        tfp->dump(contextp->time()); //dump wave
         
         for(int t=0;t<3;t++){
             contextp->timeInc(5);
             top->clk = !top->clk;
             top->eval();
-            // tfp->dump(contextp->time()); //dump wave
+            tfp->dump(contextp->time()); //dump wave
         }
         contextp->timeInc(5);
         top->clk = !top->clk;
