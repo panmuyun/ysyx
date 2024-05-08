c30733cf1684c983675e525e51a4583a94d9610c
tracer-ysyx <tracer@ysyx.org>
2024-05-08 10:38:04 +0800

    > sim RTL ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:38:04 up  1:45,  1 user,  load average: 0.38, 0.47, 0.44

--- npc/simcsrc/ps2_keyboard_sim.cpp
+++ npc/simcsrc/ps2_keyboard_sim.cpp
@@ -114,6 +114,11 @@ int main(int argc, char** argv) {
         top->ps2_clk = 1;
         top->eval();
         tfp->dump(contextp->time()); //dump wave
+
+        contextp->timeInc(5);
+        top->clk = !top->clk;
+        top->eval();
+        tfp->dump(contextp->time()); //dump wave
         // contextp->timeInc(20);
         // top->ps2_clk = 1;
         // top->eval();
