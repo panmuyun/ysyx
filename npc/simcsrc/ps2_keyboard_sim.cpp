c827d6bcd000454777b19a898d9d8798329fe262
tracer-ysyx <tracer@ysyx.org>
2024-05-08 09:09:09 +0800

    > sim RTL ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:09:09 up 16 min,  1 user,  load average: 0.33, 0.33, 0.34

--- npc/simcsrc/ps2_keyboard_sim.cpp
+++ npc/simcsrc/ps2_keyboard_sim.cpp
@@ -131,7 +131,7 @@ int main(int argc, char** argv) {
         
         // top->eval();
         // tfp->dump(contextp->time()); //dump wave
-        // printf("ps2_data = %d, data = %d, presscount = %d\n", top->ps2_data, top->data, top->presscount);
+        printf("ps2_data = %d, data = %d, presscount = %d\n", top->ps2_data, top->data, top->presscount);
         // printf("clk = %d, resetn = %d, ps2_clk = %d, ps2_data = %d\n", top->clk, top->resetn, top->ps2_clk, top->ps2_data);
         //assert(top->f == (a ^ b));
         cycle++;
