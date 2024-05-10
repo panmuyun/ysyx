b96d1ae3296800f480258ecd5762a46a160cfa20
tracer-ysyx <tracer@ysyx.org>
2024-05-10 21:52:43 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:52:43 up  1:07,  1 user,  load average: 0.27, 0.31, 0.35

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -216,7 +216,7 @@ void sdb_mainloop() {
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (cmd_table[i].handler(args) < 0) { return; }   //执行调试命令对应的功能，并控制是否退出
         break;
       }
     }
