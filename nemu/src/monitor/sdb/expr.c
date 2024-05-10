5d96089ae17fddbe8ed761f63d1919cc99da5597
tracer-ysyx <tracer@ysyx.org>
2024-05-10 22:12:36 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:12:36 up  1:26,  1 user,  load average: 0.88, 1.19, 0.86

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -298,8 +298,8 @@ EXPR_value_TYPE eval(int p, int q){
 }
 
 EXPR_value_TYPE expr(char *e, bool *success) {
-  // int length_tokens = sizeof(tokens)/sizeof(tokens[0]);
-  // resetTokens(tokens, length_tokens);
+  int length_tokens = sizeof(tokens)/sizeof(tokens[0]);
+  resetTokens(tokens, length_tokens);
 
   if (!make_token(e)) {
     *success = false;
@@ -307,17 +307,17 @@ EXPR_value_TYPE expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  // int q=0;
-  // for (int i = 0; i < length_tokens; i++){
-  //   if (tokens[i].type != 0){
-  //     q++;
-  //     //printf("tokens[%d].type = %d  ;  str = %s\n", i, tokens[i].type, tokens[i].str);
-  //   }else
-  //     break;
-  // }
+  int q=0;
+  for (int i = 0; i < length_tokens; i++){
+    if (tokens[i].type != 0){
+      q++;
+      //printf("tokens[%d].type = %d  ;  str = %s\n", i, tokens[i].type, tokens[i].str);
+    }else
+      break;
+  }
   //printf("q = %d\n", q);
 
   *success = true;
 
-  return 0;//eval(0, q-1);
+  return eval(0, q-1);
 }
