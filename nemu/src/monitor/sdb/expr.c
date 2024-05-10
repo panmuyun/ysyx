a2d8ece361da0a6b29cad850db6dd789ad92a8dc
tracer-ysyx <tracer@ysyx.org>
2024-05-10 21:59:19 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:59:19 up  1:13,  1 user,  load average: 0.40, 0.43, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -307,14 +307,14 @@ EXPR_value_TYPE expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  int q=0;
-  for (int i = 0; i < length_tokens; i++){
-    if (tokens[i].type != 0){
-      q++;
-      //printf("tokens[%d].type = %d  ;  str = %s\n", i, tokens[i].type, tokens[i].str);
-    }else
-      break;
-  }
+  // int q=0;
+  // for (int i = 0; i < length_tokens; i++){
+  //   if (tokens[i].type != 0){
+  //     q++;
+  //     //printf("tokens[%d].type = %d  ;  str = %s\n", i, tokens[i].type, tokens[i].str);
+  //   }else
+  //     break;
+  // }
   //printf("q = %d\n", q);
 
   *success = true;
