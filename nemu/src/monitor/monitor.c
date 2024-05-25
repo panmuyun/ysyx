01b4589a734143f0732c560e6464da550cb29a8a
tracer-ysyx <tracer@ysyx.org>
2024-05-25 14:55:04 +0800

    > compile NEMU ysyx_22040000 潘牧云 Linux panmy-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:04 up  3:54,  1 user,  load average: 0.47, 0.37, 0.28

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -15,6 +15,7 @@
 
 #include <isa.h>
 #include <memory/paddr.h>
+#include <stdio.h>
 
 void init_rand();
 void init_log(const char *log_file);
@@ -78,7 +79,7 @@ static int parse_args(int argc, char *argv[]) {
     {0          , 0                , NULL,  0 },
   };
   int o;
-  assert(argv!=NULL);
+  printf("@@@@@\n");
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
